c45579969ed3e4e183b42c5a67315830a256dc42
sekhar-bigtapp <chandrasekhar.g@bigtappanalytics.net>
2022-07-19 11:38:46 +0530

    forecast

--- src/app/modules/configuration/forecasted-config/forecasted-config.component.ts
+++ src/app/modules/configuration/forecasted-config/forecasted-config.component.ts
@@ -16,7 +16,7 @@ export class ForecastedConfigComponent implements OnInit {
 
   pipe = new DatePipe('en-US');
   forecastForm!: FormGroup;
-  displayColumns: string[] = ['Time_Key', 'Store_Name', 'Product_Name',  'Sales_Volume', 'p10', 'p50', 'p90', 'p10Variance', 'p50Variance', 'p90Variance'];
+  displayColumns: string[] = ['Time_Key', 'Store_Name', 'Product_Name', 'Sales_Volume', 'p10', 'p50', 'p90', 'p10Variance', 'p50Variance', 'p90Variance'];
   forecastMasterData!: MatTableDataSource<any>;
   pageSize = 10;
   isPTenValue: boolean = true;
@@ -43,6 +43,24 @@ export class ForecastedConfigComponent implements OnInit {
       SKU_ID: [''],
     });
     this.getStoresNamesList();
+    let checkbox1 = document.getElementById("inlineRadio1") as HTMLInputElement;
+    let checkbox2 = document.getElementById("inlineRadio2") as HTMLInputElement;
+    let checkbox3 = document.getElementById("inlineRadio3") as HTMLInputElement;
+    if (localStorage.getItem("P10") && localStorage.getItem("P10") == "true") {
+      checkbox1.checked = true;
+      checkbox2.checked = false;
+      checkbox3.checked = false;
+    }
+    if (localStorage.getItem("P50") && localStorage.getItem("P50") == "true") {
+      checkbox1.checked = false;
+      checkbox2.checked = true;
+      checkbox3.checked = false;
+    }
+    if (localStorage.getItem("P90") && localStorage.getItem("P90") == "true") {
+      checkbox1.checked = false;
+      checkbox2.checked = false;
+      checkbox3.checked = true;
+    }
   }
   backButtonClick() {
     this.router.navigate(['configurations']);
@@ -102,4 +120,21 @@ export class ForecastedConfigComponent implements OnInit {
     }
   }
 
+  onPValuesClick(value: any) {
+    if (value == 'P10') {
+      localStorage.setItem("P10", "true");
+      localStorage.setItem("P50", "false");
+      localStorage.setItem("P90", "false");
+    }
+    if (value == 'P50') {
+      localStorage.setItem("P10", "false");
+      localStorage.setItem("P50", "true");
+      localStorage.setItem("P90", "false");
+    }
+    if (value == 'P90') {
+      localStorage.setItem("P10", "false");
+      localStorage.setItem("P50", "false");
+      localStorage.setItem("P90", "true");
+    }
+  }
 }
